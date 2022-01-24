cbb3e35296f2ad9f44ac5c93eddfc5957fb05b62
Connor Smith <cs878@kent.ac.uk>
2022-01-24 10:55:48 +0000

    Added list and button functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Counter extends Component { 
-    state = {
-        count: 0
-    };
-    styles={
-        fontSize: 50,
-        fontWeight: "bold"
-    };
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold",
+  };
 
-    render() { 
-        return (
-            <React.Fragment>
-                <span style={this.styles} className="badge bg-primary m-2">{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
-            </React.Fragment>
-        );
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={() => this.handleIncrement(2)}
+          className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+        <div>
+          {this.state.tags.length === 0 && "Please create a new tag!"}
+          {this.renderTags()}
+        </div>
+      </React.Fragment>
+    );
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There is no tags!</p>;
     }
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
